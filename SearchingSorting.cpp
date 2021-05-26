876005e67c30a96c8a70603914519bf49de8c4cc
nazir098 <82276414+nazir098@users.noreply.github.com>
2021-05-26 14:17:58 +0530

    Create SearchingSorting.cpp

---
+++ SearchingSorting.cpp
@@ -0,0 +1,7 @@
+34. Find First and Last Position of Element in Sorted Array
+dry run on corner cases of 
+-first position 
+-last position of a num in array of repeated ele
+-index for inserting in non repeating array
+-index for inseerting in repeating array
+array are sorted
