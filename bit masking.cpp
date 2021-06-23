0c25b0a6bdc949e001116d82ad0ec306c9f38478
Md Nazir Husain <82276414+nazir098@users.noreply.github.com>
2021-06-24 01:27:25 +0530

    Create bit masking.cpp

---
+++ bit masking.cpp
@@ -0,0 +1,93 @@
+concepts:
+y=n & n-1 => remove the first one from left side 
+p=n & -n => it makes the first 1 to 1 and rest of the thing will be zero
+n= y|p =>we will get the same no
+
+bits as boolean array
+kth index, arr[k]           ==1<<k
+arr[k]=true                 == arr|= mask
+arr[k]=false                == arr &= (~mask)
+set and reset from same exp
+                               arr^=mask
+//191. Number of 1 Bits
+int hammingWeight(uint32_t n) {
+        int i=0;
+        int cnt=0;
+        while(n!=0)
+        {
+            if(n&(1))
+                cnt++;
+            n=n>>1;
+       
+        }
+        return cnt;
+    }
+    ////////////////
+ int hammingWeight(uint32_t n) {
+         int count = 0;
+        while (n != 0) 
+        {
+            count++;
+            n &= (n - 1);
+        }
+
+        return count;
+    }
+    //////////////////////////////////////////////////////////
+    338. Counting Bits
+  vector<int> countBits(int n) 
+  {      
+        vector<int>ans(n+1,0);
+        for(int i=0;i<=n;i++)
+        {
+            int div=i/2;
+            int rem=i%2;
+            ans[i]=ans[div]+rem;
+        }
+        return ans;
+    }
+    /////////
+    using bitwise opn
+    we know n & n-1 reduces no. of 1 bit by one from n
+    so a[n]=a[n & n-1];
+    countBits(int num) 
+    {    vector<int>(num + 1,0);
+        for (int i = 1; i <= num; i++) 
+        {
+            ans[i] = ans[i & (i - 1)] + 1;
+        }
+
+        return ans;
+    }
+    ///////////////////////////////////////////
+    136. Single Number
+     int singleNumber(vector<int>& nums) {
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+    /////////////////////////////////////////
+    342. Power of Four
+       bool isPowerOfFour(int n) {
+        if(n<1)return false;
+        
+        if((n &(n-1))==0)
+        {
+             int mask=1;
+            for(int i=0;i<32;i++)
+            {
+                 
+                if(((mask<<i) & n))
+                {
+                    if(i%2==0)
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    ///////////////////////////////////////////
+    
