77daec14f3731f9fcae427b4a743d0a15cf8b6fd
Md Nazir Husain <82276414+nazir098@users.noreply.github.com>
2021-07-16 01:39:40 +0530

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -30,3 +30,31 @@
         curr->next=curr->next->next;
         return dummy->next;
     }
+2. Add Two Numbers
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        if(l1==nullptr||l2==nullptr)
+            return l1==nullptr?l2:l1;
+        
+        int carry=0;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* head=dummy;
+        while(l1!=nullptr||l2!=nullptr||carry!=0)
+        {
+            int val1=l1!=nullptr?l1->val:0;
+            int val2=l2!=nullptr?l2->val:0;
+            int sum=(val1 +val2 + carry);
+            carry=sum/10;
+            cout<<sum<<" ";
+            head->next=new ListNode(sum%10);
+            
+            head=head->next;
+            if(l1!=nullptr)
+            l1=l1->next;
+            if(l2!=nullptr)
+            l2=l2->next;
+            
+        }
+        l1==nullptr?head->next=l2:head->next=l1;
+        return dummy->next;
+    }
