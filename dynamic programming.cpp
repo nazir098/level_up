fd2ad6333f431a64298faf502636569c99d4b4a9
Md Nazir Husain <82276414+nazir098@users.noreply.github.com>
2021-09-05 00:41:12 +0530

    Create dynamic programming.cpp

---
+++ dynamic programming.cpp
@@ -0,0 +1,7 @@
+backtracking m dp nhi lgti q k dp ki condition ye h k jb bhi ek number k liye call lge tree diagram m sb jgh se same ans ho ar structure bhi same ho
+# no diff bw jumping and selecting in dp hm jha pe ja rhe h us value se mtlb h
+kisi matric k shuru se le k end tk jane k kitne triqe h agr m sirf right side ar diagonal hi select kru to 
+
+#fibnocci pattern: jime 2 call lge like f(n-1) and f(n-2) 
+1.decode strig 1 
+
