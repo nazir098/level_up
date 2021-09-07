c5131ae5307e01356e76fbb5a11fa2edf8d9ef05
Md Nazir Husain <82276414+nazir098@users.noreply.github.com>
2021-09-07 11:56:50 +0530

    Update dynamic programming.cpp

--- dynamic programming.cpp
+++ dynamic programming.cpp
@@ -1,7 +1,15 @@
 backtracking m dp nhi lgti q k dp ki condition ye h k jb bhi ek number k liye call lge tree diagram m sb jgh se same ans ho ar structure bhi same ho
+
+memoization kha se start krni chahie?
+  dry run k time start kro i=0, j=0 se pr jb code krne ki baat aati h to i=m and j=n se start kr skte ho
+  kr hm dono se skte h pr agr hme memoization se tabulation m krna h to change krna kaafi easy rhega kchh dimag nhi lgana pdega
+  
 # no diff bw jumping and selecting in dp hm jha pe ja rhe h us value se mtlb h
 kisi matric k shuru se le k end tk jane k kitne triqe h agr m sirf right side ar diagonal hi select kru to 
 
 #fibnocci pattern: jime 2 call lge like f(n-1) and f(n-2) 
 1.decode strig 1 
+
+#string operation: s[i]==s[j] h to kya hoga 
+                   agr nhi h to kya hoga
 
